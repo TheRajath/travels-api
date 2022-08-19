753b9cc0c38f9d62c74cb568a0b23dfc71d6fb5f
TheRajath <sairajath7@gmail.com>
2022-08-19 20:17:23 +0530

    initial sb config

--- src/main/java/com/tourism/travels/controller/TravelsController.java
+++ src/main/java/com/tourism/travels/controller/TravelsController.java
@@ -2,16 +2,19 @@ package com.tourism.travels.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class TravelsController {
 
+    @GetMapping("/packages")
     public String getPackages(String string) {
 
         return string;
     }
 
+    @GetMapping("/totalPackages")
     public Integer getTotalPackages() {
 
         return 20;
