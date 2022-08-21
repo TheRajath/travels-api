3973e9e64eee48ccddc946f57259342d70f3b412
TheRajath <sairajath7@gmail.com>
2022-08-21 20:17:30 +0530

    service test

--- src/test/java/com/tourism/travels/packages/PackageServiceTest.java
+++ src/test/java/com/tourism/travels/packages/PackageServiceTest.java
@@ -1,5 +1,6 @@
 package com.tourism.travels.packages;
 
+import com.tourism.travels.exception.AlreadyExistsException;
 import com.tourism.travels.sql.PackageEntity;
 import com.tourism.travels.sql.PackageRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,8 +11,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -47,6 +50,41 @@ class PackageServiceTest {
             verify(packageRepository).findAll();
 
             verifyNoMoreInteractions(packageRepository);
+        }
+
+    }
+
+    @Nested
+    class AddNewPackage {
+
+        @Test
+        void works() {
+            // Arrange
+            var packageEntity = new PackageEntity();
+            packageEntity.setId(123);
+
+            // Act
+            packageService.addNewPackage(packageEntity);
+
+            // Assert
+            verify(packageRepository).findById(packageEntity.getId());
+            verify(packageRepository).save(packageEntity);
+
+            verifyNoMoreInteractions(packageRepository);
+        }
+
+        @Test
+        void throwsAlreadyExistsException_whenThereIsAnExistingRecord() {
+            // Arrange
+            var packageEntity = new PackageEntity();
+            packageEntity.setId(123);
+
+            when(packageRepository.findById(packageEntity.getId())).thenReturn(Optional.of(packageEntity));
+
+            // Act/Assert
+            assertThatThrownBy(() -> packageService.addNewPackage(packageEntity))
+                    .isInstanceOf(AlreadyExistsException.class)
+                    .hasMessage("Package with is id: 123 already exists");
         }
 
     }
